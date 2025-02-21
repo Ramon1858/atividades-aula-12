0b3a9123edb651cdf3a6dd77e00e5254b314115d
Ramon Caetano <caetanoramon678@gmail.com>
2025-02-21 16:35:37 -0300

    Add files via upload

---
+++ atividade4.py
@@ -0,0 +1,6 @@
+#Contagem regressiva simples
+#Escreva um programa que exiba uma contagem regressiva de 10 a 1, e depois imprima "Fogo!".(loop for)
+l = [10,9,8,7,6,5,4,3,2,1]
+for c in l:
+    print (c)
+print('fogo!')
