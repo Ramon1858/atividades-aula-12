d16d68e575866bb4a7014425cea2e8c76601284a
Ramon Caetano <caetanoramon678@gmail.com>
2025-02-21 16:34:28 -0300

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,8 @@
+#Crie 3 números aleatórios
+import random
+def aleatorio():
+    n1  =  random.randint(2,99)
+    n2  =  random.randint(2,99)
+    n3  =  random.randint(2,99)
+    print(n1,n2,n3)  
+aleatorio()
